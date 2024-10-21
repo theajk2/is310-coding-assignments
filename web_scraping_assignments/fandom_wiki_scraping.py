20fcd746b0a572d94bda78d558c720c397a53856
“theajk2” <theajk2@illinois.edu>
2024-10-21 14:25:40 -0500

    fandom wiki scraping

--- web_scraping_assignments/fandom_wiki_scraping.py
+++ web_scraping_assignments/fandom_wiki_scraping.py
@@ -6,7 +6,7 @@ import csv
 url = "https://vampirechronicles.fandom.com/wiki/Category:Characters_(Interview_with_the_Vampire_TV)"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-character= soup.find_all("a", class_= 'category-page__member-link')
+characters = soup.find_all("a", class_= 'category-page__member-link')
 
 #with open('iwtv-characters.csv',mode = 'w',newline='', encoding = 'utf-8')as file:
     #writer = csv.writer(file)
@@ -16,18 +16,24 @@ with open('iwtv-characters.csv',mode = 'w',newline='', encoding = 'utf-8')as fil
         writer = csv.writer(file)
         writer.writerow(['name','status'])
 
-for i in character:
+for character in characters:
     #name = character.find('a',class_='Category:Characters').text
+    #name = soup.select_one('h1').text.strip()
     name = character.get_text()
-    status = i.find('span', class_='Category:Characters')
-    if 'alive' in status.lower():
+    status = character.find('span', class_='Category:Characters')
+    if status == "alive":
         #aliveChar.append(name)
         status = "alive"
-        writer.writerow([name, status])
+        #writer.writerow([name, status])
     else:
         #deadChar.append(name)
         status = "dead"
-        writer.writerow([name, status])
+        #writer.writerow([name, status])
+    with open('iwtv-characters.csv',mode = 'w',newline='', encoding = 'utf-8')as file:
+        writer = csv.writer(file)
+        writer.writerow(['name','status'])
+        writer.writerow([name,status])
+   
     # for index, row in writer.writerows('iwtv-characters.csv'):
                
 
