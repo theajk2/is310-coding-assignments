27fd7a24f5ee0b8d4c242f32763590a6efcde21c
“theajk2” <theajk2@illinois.edu>
2024-10-21 22:36:39 -0500

    is310 assignment

--- command-line-data-curation/cli_data_entry.py
+++ command-line-data-curation/cli_data_entry.py
@@ -3,18 +3,17 @@ from rich.table import Table
 
 console = Console()
 console.print("Here are a couple books that I love:", style ="purple")
-table.add_column("Title", no_wrap = True)
-table.add_column("Genre", no_wrap = True)
-table.add_column("Year Published", no_wrap = True)
-table.add_row("The Priory of the Orange Tree", "fantasy", "2019")
-table.add_row("Challenger Deep","realistic fiction", "2016")
-table.add_row("Blue Lily, Lily Blue", "magical realism","2014")
-console.print(table)
+Table.add_column("Title", no_wrap = True)
+Table.add_column("Genre", no_wrap = True)
+Table.add_column("Year Published", no_wrap = True).add_row("The Priory of the Orange Tree", "fantasy", "2019")
+Table.add_row("Challenger Deep","realistic fiction", "2016")
+Table.add_row("Blue Lily, Lily Blue", "magical realism","2014")
+console.print(Table)
 console.print("\n[bold orange]What's your favorite book? Input them:[/bold orange]")
 def save_text(table):
       var = open('book_list.txt', 'a')  
-      for row_index in range(len(table.columns[0]._cells)):
-              content = [str(column._cells[row_index]) for column in table.columns]
+      for row_index in range(len(table.column[0]._cells)):
+              content = [str(column._cells[row_index]) for column in table.column]
               var.write("".join(content) + "\n")
               print(content)
               
